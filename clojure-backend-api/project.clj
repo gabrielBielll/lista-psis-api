b95d041b7660d4f3de76d406aa50c8a06a51c934
gabrielBielll <gabrielbarbosa.ff@gmail.com>
2025-06-19 10:33:19 -0300

    Update project.clj

--- clojure-backend-api/project.clj
+++ clojure-backend-api/project.clj
@@ -1,13 +1,28 @@
 (defproject clojure-backend-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "API para gerenciar horários de psicólogas."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring-core "1.9.6"]
                  [ring/ring-jetty-adapter "1.9.6"]
-                 [compojure "1.7.0"]]
+                 [ring/ring-json "0.5.1"]
+                 [compojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.5.0"]
+                 [environ "1.2.0"]
+                 [crypto-password "0.3.0"]
+                 [cheshire "5.11.0"]]
+
   :main ^:skip-aot clojure-backend-api.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  
+  :plugins [[lein-ring "0.12.6"]]
+
+  :ring {:handler clojure-backend-api.core/app
+         :init clojure-backend-api.core/init
+         :destroy clojure-backend-api.core/destroy}
+  
+  :profiles
+  {:uberjar {:aot :all
+             :uberjar-name "clojure-backend-api.jar"}})
