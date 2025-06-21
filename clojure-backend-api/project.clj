6c19afd12992cfefa37c7d13f291988ec1b60316
gabrielBielll <gabrielbarbosa.ff@gmail.com>
2025-06-21 11:13:53 -0300

    Update project.clj

--- clojure-backend-api/project.clj
+++ clojure-backend-api/project.clj
@@ -14,9 +14,9 @@
                  [buddy/buddy-hashers "1.8.1"]
                  [cheshire "5.11.0"]
                  [ring-cors "0.1.13"]
-                 ;; --- NOVA BIBLIOTECA DE MÉTRICAS ---
+                 ;; --- NOVA BIBLIOTECA DE MÉTRICAS (CORRIGIDO) ---
                  [metrics-clojure "2.10.0"]
-                 [metrics-clojure-prometheus "2.10.0"]]
+                 [metrics-clojure/metrics-clojure-prometheus "2.10.0"]] ; <-- CORRIGIDO AQUI
 
   :main ^:skip-aot clojure-backend-api.core
   :target-path "target/%s"
